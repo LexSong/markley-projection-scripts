85465317bb12c3adf1d28fcfce62db3e69a8129d
Lex Song <LexSong.tw@gmail.com>
2021-06-27 00:44:32 +0800

    Add tetrahedralMarkley.js
    Reference: https://observablehq.com/@fil/markley

---
+++ tetrahedralMarkley.js
@@ -0,0 +1,16 @@
+import { geoTetrahedralLee } from "d3-geo-polygon";
+
+// Singularities are at latitudes 35.26439
+const latitude = (Math.acos(1 / 3) / 2 / Math.PI) * 180;
+const rotate = [180, latitude - 90, 180];
+
+const tetrahedralMarkley = geoTetrahedralLee()
+  .rotate(rotate)
+  .fitWidth(2, {
+    type: "MultiPoint",
+    coordinates: [
+      [0, 90],
+      [-90, 0],
+      [90, 0],
+    ],
+  });
